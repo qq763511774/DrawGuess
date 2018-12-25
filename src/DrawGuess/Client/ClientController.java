8f71511981cc9638b8b0d06b6e1de82eaa05de53
FrzA-OuO <37104795+FrzA-OuO@users.noreply.github.com>
2018-12-26 01:30:43 +0800

    overwrite client

---
+++ src/DrawGuess/Client/ClientController.java
@@ -0,0 +1,9 @@
+package DrawGuess.Client;
+
+public class ClientController {
+    public ClientController(ClientUI client){
+
+    }
+
+    public void dealwith(String IPAddress, String username){}
+}
