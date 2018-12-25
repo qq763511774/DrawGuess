4de3db399e0d4f555715f43417d97f115cbc25a0
FrzA-OuO <37104795+FrzA-OuO@users.noreply.github.com>
2018-12-26 02:37:17 +0800

    update client

--- src/DrawGuess/Client/ClientController.java
+++ src/DrawGuess/Client/ClientController.java
@@ -1,9 +1,45 @@
 package DrawGuess.Client;
 
+import DrawGuess.Bag;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 public class ClientController {
-    public ClientController(ClientUI client){
-
+    ClientUI ui;
+    public ClientController( ClientUI clientUI ){
+        ui = clientUI;
     }
+    // Test part
+    public void dealwith(String IPAddress, String username){
+        try {
+            Socket socket = new Socket(IPAddress, 8765);
 
-    public void dealwith(String IPAddress, String username){}
+            Bag bag = new Bag(username, "Hello");
+
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+
+            objectOutputStream.writeObject(bag);
+            objectOutputStream.flush();
+
+            // ObjectInputStream objectInputStream = new ObjectInputStream( new BufferedInputStream(socket.getInputStream()));
+            ObjectInputStream objectInputStream = new ObjectInputStream( socket.getInputStream());
+            while( true ) {
+
+                Object obj = objectInputStream.readObject();
+                if (obj != null) {
+                    Bag bag1 = (Bag) obj;
+                    System.out.println(bag1.name + ": " + bag1.message);
+                }
+
+            }
+
+        }catch (IOException e){
+
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+    }
 }
